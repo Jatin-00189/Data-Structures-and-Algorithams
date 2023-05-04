6fdd61291f424cd4dac83d2c9f4b4c933c94d503
0X54nj4Y_j4nGr4 <106859771+Jatin-00189@users.noreply.github.com>
2023-05-04 02:40:49 -1200

    Create Insertion_at_end_In_Linked-Lists.c

---
+++ Insertion_at_end_In_Linked-Lists.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* insert(struct Node* head,int data) {
+
+    struct Node* new_node=NULL;
+    new_node = (struct Node*)malloc(sizeof(struct Node));
+
+    new_node->data = data;
+    new_node->next = NULL;
+
+    if(head==NULL) {
+        head = new_node;
+    }else {
+        struct Node* temp=head;
+
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+    return head;
+}
+
+int main() {
+    struct Node* head=NULL;
+
+    head=insert(head,10);
+    head=insert(head,20);
+    head=insert(head,30);
+    head=insert(head,50);
+    head=insert(head,40);
+
+
+    struct Node* temp=head;
+
+    while(temp->next!=NULL) {
+        printf("%d",temp->data);
+        temp=temp->next;
+    }
+
+
+    return (0);
+
+
+}
